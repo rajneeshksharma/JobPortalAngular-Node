0d7b8a16ffa1325f415a8f58a87ed6dbe0166b8f
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-09-25 16:53:52 +0530

    Register Done Only front not  Form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,27 +1,28 @@
-import { Routes, RouterModule } from "@angular/router";
-import { HomepageComponent } from "./homepage/homepage.component";
-import { NgModule } from "@angular/core";
-import { JobsComponent } from "./jobs/jobhome/jobs.component";
-import { ContactusComponent } from "./contactus/contactus.component";
-import { AboutusComponent } from "./aboutus/aboutus.component";
-import { LoginComponent } from "./login/login.component";
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NgModule } from '@angular/core';
+// import { JobsComponent } from './jobs/jobhome/jobs.component';
+import { ContactusComponent } from './contactus/contactus.component';
+// import { AboutusComponent } from './aboutus/aboutus.component';
+// import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-    {path: '', redirectTo:'/index', pathMatch:'full'},
-    {path: 'index', component: HomepageComponent},
-    {path: 'contactus', component: ContactusComponent},
-    {path: 'aboutus', loadChildren: './aboutus/aboutus.module#AboutusModule'},
-    {path: 'login', loadChildren:'./login/login.module#LoginModule'},
-    {path: 'job', loadChildren: './jobs/job.module#JobModule'}
+    { path: '', redirectTo: '/index', pathMatch: 'full' },
+    { path: 'index', component: HomepageComponent },
+    { path: 'contactus', component: ContactusComponent },
+    { path: 'aboutus', loadChildren: './aboutus/aboutus.module#AboutusModule' },
+    { path: 'login', loadChildren: './login/login.module#LoginModule' },
+    { path: 'job', loadChildren: './jobs/job.module#JobModule' },
+    { path: 'register', loadChildren: './register/register.module#RegisterModule' }
 ];
 
 @NgModule({
-    imports:[
+    imports: [
         RouterModule.forRoot(routes)
     ],
-    exports:[
+    exports: [
         RouterModule
     ]
-    
+
 })
-export class AppRouterMoudle{}
+export class AppRouterMoudle { }
