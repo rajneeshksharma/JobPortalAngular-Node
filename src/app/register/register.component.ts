78a83855dbe90ffd682bd6b414bd352b1b713a4e
rajneeshksharma <rajneeshksharmaoffice@gmail.com>
2018-09-26 11:27:48 +0530

    Signup form Done with Rective forms and validation @rajneesh

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder , Validators , FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+      this.signupForm = this.formBuilder.group({
+          userType: ['', Validators.required],
+          name: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          gender : ['', Validators.required],
+          Pnumber : ['', [Validators.required, Validators.minLength(10)]],
+          password: ['', [
+              Validators.required,
+              Validators.minLength(8)
+          ]],
+          cpassword: ['', [
+              Validators.required,
+              Validators.minLength(8)
+          ]],
+      }, { validator: this.pwdMatchValidator
+
+  });
+
+}
+pwdMatchValidator(frm: FormGroup) {
+  return frm.get('password').value === frm.get('cpassword').value
+     ? null : {'mismatch': true};
+}
+
+get f() { return this.signupForm.controls; }
+
+onSubmit() {
+  this.submitted = true;
+  console.log(this.signupForm);
+  // stop here if form is invalid
+  if (this.signupForm.invalid) {
+      return;
   }
+  alert(JSON.stringify(this.signupForm.value));
+  console.log(this.signupForm.value);
+  // alert(' User Email - ' + this.signupForm.value.email + ' User Password - ' + this.signupForm.value.password);
+  // console.log(' User Email - ' + this.signupForm.value.email + ' User Password - ' + this.signupForm.value.password );
+}
+
 
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RegisterRoutingModule } from './register-routing.module';
 import { RegisterComponent } from './register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   imports: [
-    RegisterRoutingModule
+    RegisterRoutingModule,
+    ReactiveFormsModule, FormsModule,
+    CommonModule
   ],
   declarations: [RegisterComponent]
 })
